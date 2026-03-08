aa8a08a4c1adc483517fce78e0f83745e96bd027
Ramish Adeel <129926032+ramisxh@users.noreply.github.com>
2026-03-08 13:10:32 +0530

    Fix base path configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/luna-health/',  // 👈 add this
   plugins: [react()],
-  base: '/luna-health/',
 })
